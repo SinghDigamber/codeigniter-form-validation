bbba8afcc37c81891edb6797ec117a3c1361abee
Digamber Rawat <digambersingh126@gmail.com>
2022-09-10 00:07:13 +0530

    Update FormController.php

--- app/Controllers/FormController.php
+++ app/Controllers/FormController.php
@@ -6,34 +6,36 @@ use CodeIgniter\Controller;
 class FormController extends Controller
 {
 
-    public function create() {
+    public function index() {
+        helper(['form']);
+        $data = [];
         return view('contact_form');
     }
  
-    public function formValidation() {
-        helper(['form', 'url']);
-        
-        $input = $this->validate([
+    public function store() {
+        helper(['form']);
+        $rules = [
             'name' => 'required|min_length[3]',
             'email' => 'required|valid_email',
             'phone' => 'required|numeric|max_length[10]'
-        ]);
+        ];
+          
+        if($this->validate($rules)){
+            $formModel = new FormModel();
 
-        $formModel = new FormModel();
- 
-        if (!$input) {
-            echo view('contact_form', [
-                'validation' => $this->validator
-            ]);
-        } else {
-            $formModel->save([
+            $data = [
                 'name' => $this->request->getVar('name'),
                 'email'  => $this->request->getVar('email'),
                 'phone'  => $this->request->getVar('phone'),
-            ]);          
+            ];
 
-            return $this->response->redirect(site_url('/submit-form'));
-        }
+            $formModel->save($data);
+
+            return redirect()->to('/contact-form');
+        }else{
+            $data['validation'] = $this->validator;
+            echo view('contact_form', $data);
+        }        
     }
 
 }
